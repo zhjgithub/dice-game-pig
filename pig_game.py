f528522c331e6d699bda1acde9f3653e3b09b2d4
organism <neuqzhj@live.com>
2018-01-09 13:03:27 +0800

    refactor: Back to tuple

--- pig_game.py
+++ pig_game.py
@@ -6,7 +6,6 @@ from collections import namedtuple
 
 other = [1, 0]
 goal = 50
-State = namedtuple('State', 'p me you pending')
 
 # States are represented as a tuple of (p, me, you, pending) where
 # p:       an int, 0 or 1, indicating which player's turn it is.
@@ -18,15 +17,16 @@ State = namedtuple('State', 'p me you pending')
 def hold(state):
     """Apply the hold action to a state to yield a new state:
     Reap the 'pending' points and it becomes the other player's turn."""
-    return other[state.p], state.you, state.me + state.pending, 0
+    p, me, you, pending = state
+    return other[p], you, me + pending, 0
 
 
 def roll(state, d):
     """Apply the roll action to a state (and a die roll d) to yield a new state:
     If d is 1, get 1 point (losing any accumulated 'pending' points),
     and it is the other player's turn. If d > 1, add d to 'pending' points."""
-    return (other[state.p], state.you, state.me + d,
-            0) if d == 1 else (state.p, state.me, state.you, state.pending + d)
+    p, me, you, pending = state
+    return (other[p], you, me + d, 0) if d == 1 else (p, me, you, pending + d)
 
 
 def hold_at(x):
@@ -34,8 +34,8 @@ def hold_at(x):
     pending >= x or player reaches goal."""
 
     def strategy(state):
-        s = State(*state)
-        return 'hold' if s.pending >= x or s.me + s.pending >= goal else 'roll'
+        _, me, _, pending = state
+        return 'hold' if pending >= x or me + pending >= goal else 'roll'
 
     strategy.__name__ = 'hold_at(%d)' % x
     return strategy
@@ -43,10 +43,10 @@ def hold_at(x):
 
 def test():
     "tests."
-    assert hold(State(1, 10, 20, 7)) == State(0, 20, 17, 0)
-    assert hold(State(0, 5, 15, 10)) == State(1, 15, 15, 0)
-    assert roll(State(1, 10, 20, 7), 1) == State(0, 20, 11, 0)
-    assert roll(State(0, 5, 15, 10), 5) == State(0, 5, 15, 15)
+    assert hold((1, 10, 20, 7)) == (0, 20, 17, 0)
+    assert hold((0, 5, 15, 10)) == (1, 15, 15, 0)
+    assert roll((1, 10, 20, 7), 1) == (0, 20, 11, 0)
+    assert roll((0, 5, 15, 10), 5) == (0, 5, 15, 15)
 
     assert hold_at(30)((1, 29, 15, 20)) == 'roll'
     assert hold_at(30)((1, 29, 15, 21)) == 'hold'
