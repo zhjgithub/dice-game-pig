663de4ff486d1c9f9bb5118eb1ea38476e2a45e1
organism <neuqzhj@live.com>
2018-01-09 23:21:46 +0800

    feature: Add report function to display reason of probability

--- conditional_probability.py
+++ conditional_probability.py
@@ -13,13 +13,13 @@ def product(*variables):
     return map(''.join, itertools.product(*variables))
 
 
-two_kids = product(sex, sex)
+two_kids = list(product(sex, sex))
 one_boy = [s for s in two_kids if 'B' in s]
 
 # Out of all families with two kids with at least oen boy on a Tuesday,
 # what is the probability of two boys?
 day = 'SMTWtFs'  # days of week
-two_kids_bday = product(sex, day, sex, day)
+two_kids_bday = list(product(sex, day, sex, day))
 boy_tuesday = [s for s in two_kids_bday if 'BT' in s]
 
 
@@ -35,6 +35,33 @@ def condP(predicate, event):
     return Fraction(len(pred), len(event))
 
 
+boy_anyday = [s for s in two_kids_bday if 'B' in s]
+month = 'JFMAmjLaSOND'
+two_kids_bmonth = product(sex, month, sex, month)
+boy_december = [s for s in two_kids_bmonth if 'BD' in s]
+
+
+def report(verbose=False,
+           predicate=two_boys,
+           predname='2 boys',
+           cases=[('2 kids', two_kids), ('2 kids born any day', two_kids_bday),
+                  ('at least 1 boy',
+                   one_boy), ('at least 1 boy born any day', boy_anyday), (
+                       'at least 1 boy born on Tuesday', boy_tuesday), (
+                           'at least 1 boy born in December', boy_december)]):
+    import textwrap
+    for name, event in cases:
+        print(
+            'P({} | {}) = {}'.format(predname, name, condP(predicate, event)))
+        if verbose:
+            print('Reason:\n"{}" has {} elements:\n{}'.format(
+                name, len(event), textwrap.fill(' '.join(event), 85)))
+            good = [s for s in event if predicate(s)]
+            print('of those, {} are "{}":\n{}\n\n'.format(
+                len(good), predname, textwrap.fill(' '.join(good), 85)))
+
+
 if __name__ == '__main__':
     print(condP(two_boys, one_boy))
     print(condP(two_boys, boy_tuesday))
+    report(True)
