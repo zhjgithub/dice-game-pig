ed2ab9d13725b04323912cba8d220833cbff87b4
organism <neuqzhj@live.com>
2018-01-09 22:09:02 +0800

    feature: Add conditional probability module

---
+++ conditional_probability.py
@@ -0,0 +1,33 @@
+'''
+Conditional probability.
+'''
+
+import itertools
+from fractions import Fraction
+
+sex = 'BG'
+
+
+def product(*variables):
+    "The cartesian product (as a str) of the possibilities of each variable."
+    return map(''.join, itertools.product(*variables))
+
+
+two_kids = product(sex, sex)
+one_boy = [s for s in two_kids if 'B' in s]
+
+
+def two_boys(s):
+    return s.count('B') == 2
+
+
+def condP(predicate, event):
+    '''Conditional probability: P(predicate(s) | s in event).
+    The proportion of states in event for which predicate is true.
+    '''
+    pred = [s for s in event if predicate(s)]
+    return Fraction(len(pred), len(event))
+
+
+if __name__ == '__main__':
+    print(condP(two_boys, one_boy))
